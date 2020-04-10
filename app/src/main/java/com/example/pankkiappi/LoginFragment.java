d9fc5952ac9431ea91574ea45b28cd0d32437541
Siirabs <leevi.raassina@gmail.com>
2020-04-10 21:26:37 +0300

    moi

--- app/src/main/java/com/example/pankkiappi/LoginFragment.java
+++ app/src/main/java/com/example/pankkiappi/LoginFragment.java
@@ -1,6 +1,21 @@
 package com.example.pankkiappi;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
+
+    private Bundle bundle;
+    private String username;
+    private String passwword;
+
+    private EditText editUsername;
+    private EditText editPassword;
+    private Button logIn;
+    private CheckBox rememberAcc;
+    private Button createAcc;
 }
