6dfb804ed2c17fcf8e91c9ad3135e85211fe9e77
Derostry <derostry@hotmail.com>
2020-04-29 15:21:18 +0300

    Lisätty tietokantaan user_type tunniste, jolla tunnistetaan admin käyttäjä Lisätty myös digitCode.java tiedostoon käyttäjän user_type tarkistus jollla määritetään mikä activity käynnistetään

--- app/src/main/java/com/example/pankkiappi/activities/digitCode.java
+++ app/src/main/java/com/example/pankkiappi/activities/digitCode.java
@@ -26,7 +26,7 @@ import javax.xml.datatype.Duration;
 
 public class digitCode extends AppCompatActivity {
     private final AppCompatActivity activity = digitCode.this;
-
+    private DatabaseHelper databaseHelper;
     EditText editText;
 
     @Override
@@ -36,23 +36,26 @@ public class digitCode extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
-
+        initObjects();
         random();
 
     }
 
+    private void initObjects() {
+        databaseHelper = new DatabaseHelper(activity);
+    }
 
     public void random() {
         final SecureRandom random = new SecureRandom();
         int randomNumber = random.nextInt(999999);
 
-        Snackbar snackbar = Snackbar.make(findViewById(R.id.top_coordinator), "Your verification code is " + String.format("%06d",randomNumber) , Snackbar.LENGTH_INDEFINITE);
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.top_coordinator), "Your verification code is " + String.format("%06d", randomNumber), Snackbar.LENGTH_INDEFINITE);
 
         snackbar.show();
         //Toast toast = Toast.makeText(this, "Your verification code is " + randomNumber, Toast.LENGTH_SHORT);
-       // toast.setGravity(Gravity.TOP,0,0);
+        // toast.setGravity(Gravity.TOP,0,0);
         //toast.show();
-        final String randomString = String.format("%06d",randomNumber);
+        final String randomString = String.format("%06d", randomNumber);
 
         final android.widget.Button button = (android.widget.Button) findViewById(R.id.codeBtn);
         button.setOnClickListener(new View.OnClickListener() {
@@ -60,26 +63,28 @@ public class digitCode extends AppCompatActivity {
 
                 editText = (EditText) findViewById(R.id.digitCode);
                 final String temp = editText.getText().toString();
-                System.out.println(randomString);
                 System.out.println(temp);
+                Bundle bundle = getIntent().getExtras();
+                String email = bundle.getString("EMAIL");
 
                 if (randomString.equals(temp)) {
-                    Intent drawerIntent = new Intent(activity, DrawerActivity.class);
-                    startActivity(drawerIntent);
-                } else {
-                    editText.setText(null);
-                    Toast toast= Toast.makeText(getApplicationContext(),
-                            "Try again", Toast.LENGTH_SHORT);
-                    toast.show();
-                    random();
-
-
+                        if (!databaseHelper.checkAdmin(email)) {
+                            Intent drawerIntent = new Intent(activity, DrawerActivity.class);
+                            System.out.println("user paneeli");
+                            startActivity(drawerIntent);
+                        } else {
+                            Intent drawerIntent = new Intent(activity, DrawerActivity.class);
+                            System.out.println("admin paneeli");
+                            startActivity(drawerIntent);
+                        }
+                    } else {
+                        editText.setText(null);
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Try again", Toast.LENGTH_SHORT);
+                        toast.show();
+                        random();
                 }
             }
         });
-
-
     }
-
-
 }
